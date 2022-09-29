58e3b866baad8a338fb5b4384114b99049c5fe15
likelike5115 <likelike5115@gmail.com>
2022-09-28 22:35:35 -0400

    test

---
+++ birds/test/BirdTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @Description
+ * @Author
+ */
+public class BirdTest {
+    private Bird bird;
+
+    @Before
+    public void setUp(){
+        bird = new Bird();
+    }
+
+    @Test
+    public testBird(){
+        assertEquals()
+    }
+}
