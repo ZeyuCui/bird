24218d40cf514607c6ad566455d20a276f426243
Zeyu Cui <cui.ze@northeastern.edu>
2022-10-05 19:03:18 -0700

    bird modified

--- birds/src/Bird.java
+++ birds/src/Bird.java
@@ -1,9 +1,20 @@
 public interface Bird {
+
+    //get name of the bird
     public String getName();
 
+    //get type of the bird
+    public String getType();
+
+    //get category of the bird
+    public String getCategory();
+
+    //get characteristic of the bird
     public String getCharacteristic();
 
+    //get the number of the wings
     public Integer getWings();
 
+    //get the whether the bird is extinct
     public String getIsExtinct();
 }
--- birds/src/BirdClass.java
+++ birds/src/BirdClass.java
@@ -1,26 +1,72 @@
+import java.util.Map;
+
 /**
  * @Description
  * @Author
  */
-public class BirdClass {
+public class BirdClass implements Bird{
+
+    //parent class of all the different type of birds
+
+    //name of that bird
     private String name;
+
+    //type of that bird
+    private String type;
+
+    //category of that bird
+    private String category;
+
+    //characteristic of that bird
     private String characteristic;
+
+    //number of the wings the bird has
     private Integer wings;
+
+    //whether the bird is extinct
     private String isExtinct;
 
-    public BirdClass(String name, String characteristic, Integer wings, String isExtinct) {
-        this.wings = wings;
-        this.name = name;
-        this.characteristic = characteristic;
-        this.isExtinct = isExtinct;
-
-    }
+    //what food needs to be kept and in what quantities for that bird in one day
+    private Map<String,Integer> foodMap;
 
     public BirdClass() {
     }
 
+    public BirdClass(String name) {
+        this.name = name;
+    }
+
+
+    public BirdClass(String category, String characteristic, Integer wings, String isExtinct) {
+        this.wings = wings;
+        this.category = category;
+        this.characteristic = characteristic;
+        this.isExtinct = isExtinct;
+    }
+
+
+    public BirdClass(String name, String type, String category, String characteristic, Integer wings, String isExtinct, Map<String, Integer> foodMap) {
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.characteristic = characteristic;
+        this.wings = wings;
+        this.isExtinct = isExtinct;
+        this.foodMap = foodMap;
+    }
+
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String getCategory() {
+        return category;
     }
 
     public String getCharacteristic() {
@@ -50,5 +96,21 @@ public class BirdClass {
 
     public void setIsExtinct(String isExtinct) {
         this.isExtinct = isExtinct;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Map<String, Integer> getFoodMap() {
+        return foodMap;
+    }
+
+    public void setFoodMap(Map<String, Integer> foodMap) {
+        this.foodMap = foodMap;
     }
 }
