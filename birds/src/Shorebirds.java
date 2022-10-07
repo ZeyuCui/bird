3a9f2ee7ce6eb254c4cac262b445ed820bc5e9f8
Zeyu Cui <cui.ze@northeastern.edu>
2022-10-06 22:09:07 -0700

    edited main and fixed errors in shorebirds

--- birds/src/Shorebirds.java
+++ birds/src/Shorebirds.java
@@ -16,8 +16,8 @@ public class Shorebirds extends BirdsNearWater {
         }else{
             super.setExtinct(false);
         }
-        if (type != BirdsType.HornedPuffin && type != BirdsType.AfricanJacana){
-            throw new IllegalArgumentException("this bird doesn't belong to parrot's bird type");
+        if (type != BirdsType.HornedPuffin && type != BirdsType.AfricanJacana&&type!=BirdsType.GreatAuk){
+            throw new IllegalArgumentException("this bird doesn't belong to shorebird's bird type");
         }
         super.setType(type);
         super.setCategory(Category.SHOREBIRDS);
@@ -33,17 +33,21 @@ public class Shorebirds extends BirdsNearWater {
         super(waterType);
         if(type == BirdsType.GreatAuk){
             super.setExtinct(true);
-            throw new RuntimeException("An extinct bird type cannot be added into the aviary");
         }else{
             super.setExtinct(false);
         }
-        if (type != BirdsType.HornedPuffin && type != BirdsType.AfricanJacana){
-            throw new IllegalArgumentException("this bird doesn't belong to shore bird's bird type");
+        if (type != BirdsType.HornedPuffin && type != BirdsType.AfricanJacana&&type!=BirdsType.GreatAuk){
+            throw new IllegalArgumentException("this bird doesn't belong to shorebird's bird type");
         }
         super.setType(type);
         super.setCategory(Category.SHOREBIRDS);
         super.setWings(2);
         super.setCharacteristic(characteristic);
+        Map food=new HashMap<Food,Integer>();
+        food.put(Food.FISH,5);
+        food.put(Food.INSECTS,2);
+        food.put(Food.AQUATIC_INVERTEBRATES,3);
+        super.setFoodMap(food);
     }
 
 }
