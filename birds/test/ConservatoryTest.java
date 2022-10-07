52507ed7902e2c9240cacdf35b66858d708fd836
likelike5115 <likelike5115@gmail.com>
2022-10-06 21:57:38 -0400

    dev

--- birds/test/ConservatoryTest.java
+++ birds/test/ConservatoryTest.java
@@ -5,8 +5,19 @@ import org.junit.Before;
  * @Author
  */
 public class ConservatoryTest {
+    private BirdClass bird1;
+    private BirdClass bird2;
+    private BirdClass bird4;
+    private BirdClass bird5;
+    private  Aviary aviary1;
+    private  Conservatory conservatory1;
     @Before
     public void setUp(){
-
+        bird1 = new Owl();
+        bird2 = new Parrot(BirdsType.GrayParrot);
+        bird4 = new Shorebirds(BirdsType.HornedPuffin);
+        bird5 = new FlightlessBird(BirdsType.Emus);
+        aviary1 = new Aviary();
+        conservatory1 = new Conservatory();
     }
 }
